1a3fee14fbd939d41fcef46b96f35e60c8fea07e
Demetrious Young <zendo425@gmail.com>
2018-01-08 23:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return Youranimal();
 }
-
+const two = 2;
 function add2(n) {
-  return n + two
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
